990e64145c9d8f127002044c86dd6ea33d751fd4
Patrick Robinson <112195473+Prob19@users.noreply.github.com>
2023-04-13 12:21:33 -0500

    Add files via upload

--- ProgrammingProject.py
+++ ProgrammingProject.py
@@ -20,7 +20,7 @@ teamsPPGAverage={Timberwolves:{"Karl Anthony Towns":25,"Rudy Gobert": 18,"Anthon
 """
 
 teamsWL={"Timberwolves":{"Wins":0,"Losses":0},"Nets":{"Wins":0,"Losses":0},"Clippers":{"Wins":0,"Losses":0},"Magic":{"Wins":0,"Losses":0},"Kings":{"Wins":0,"Losses":0},"Heat":{"Wins":0,"Losses":0}}
-"""
+
 while True:
     team1=input("Enter a team or type 'break' for both teams to quit: ")
     team2=input("Enter another team: ")
@@ -47,55 +47,3 @@ while True:
             print(team1Score, "-", team2Score)
             print(team2, "win!")
             print(teamsWL)
-"""
-class BasketballGame:
-    def __init__(self, opponent_team):
-        self.opponent_team = opponent_team
-        self.my_team = []
-
-    def create_team(self):
-        for i in range(3):
-            player_name = input("Enter player name: ")
-            self.my_team.append(player_name)
-            i+=1
-
-    def play_game(self):
-        print(f"My Team vs {self.opponent_team}")
-        team1Score = 0
-        team2Score = 0
-        team1Score = random.randint(70, 120)
-        team2Score = random.randint(70, 120)
-        if team1Score > team2Score:
-            teamsWL[team1]["Wins"] += 1
-            teamsWL[team2]["Losses"] += 1
-            print(team1Score, "-", team2Score)
-            print(team1, "win!")
-            print(teamsWL)
-        else:
-            teamsWL[team2]["Wins"] += 1
-            teamsWL[team1]["Losses"] += 1
-            print(team1Score, "-", team2Score)
-            print(team2, "win!")
-            print(teamsWL)
-
-# Pre-defined opponent teams
-opponent_teams = {
-    "Timberwolves": ["Karl Anthony-Towns", "Rudy Gobert", "Anthony Edwards"],
-    "Nets": ["Nic Claxton", "Cam Thomas", "Seth Curry"],
-    "Clippers": ["Paul George", "Kawhi Leonard", "Marcus Morris"],
-    "Magic": ["Jalen Suggs", "Franz Wagner", "Cole Anthony"],
-    "Kings": ["Damontis Sabonis", "Kevin Huerter", "DeAaron Fox"],
-    "Heat": ["Jimmy Butler", "Tyler Herro", "Bam Adebayo"]
-}
-
-# Prompt the user to choose an opponent team
-print("Choose an opponent team:")
-for team_name in opponent_teams.keys():
-    print(f"{team_name}")
-choice = input("Enter team name: ")
-opponent_team = opponent_teams[choice]
-
-# Play the game
-game = BasketballGame(opponent_team)
-game.create_team()
-game.play_game()
