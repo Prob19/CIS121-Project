a67b5a58b6ceb51c00ce37fddc249b3e195d2d8f
Andrew <114971062+Andrew-Jungclaus@users.noreply.github.com>
2023-04-20 13:34:11 -0500

    Update ProjectV3.py

--- ProjectV3.py
+++ ProjectV3.py
@@ -22,7 +22,7 @@ class BasketballGame:
             player_name = input(f"Enter player {i} name: ")
             self.my_team.append(BasketballPlayer(player_name))
     def Team_play_game(self):
-        print(f"{self.team_name} vs {self.opponent_team}")
+        print(f"MSUM vs {self.opponent_team}")
         for player in self.my_team:
             player.Player_play_game() #calls player play game method to get a random number to set as each players points scored
             opponent_player = random.choice(self.opponent_team)#picks random player from the oppenent team
@@ -36,7 +36,7 @@ class BasketballGame:
         else:
             print(self.opponent_team, "wins")#prints if opponent team has higher integer
         f=open("newFile.txt","w") #writes team scores on an external text file
-        f.write(f"{my_team_score} - {opponent_team_score}") #IO FILE HANDLING
+        f.write(f"{my_team_score} - {opponent_team_score}")
         f.close()
 #Hard coded teams with players
 opponent_teams = {
@@ -53,10 +53,9 @@ for i in range(3): #prompts user to input 3 players
     player_name = input(f"Enter player {i+1} name: ")
     my_team.append(BasketballPlayer(player_name, "My Team")) #adds created players to the created team
 # Prompt the user to choose an opponent team
-print("\nChoose an opponent team:")
 for team_name in opponent_teams.keys():
     print(team_name)#prints the options for the teams you can pick to go against
-choice = input("Enter team name: ")#user picks opponent team
+choice = input("Enter a team to face: ")#user picks opponent team
 opponent_team = opponent_teams[choice]#finds key that matches the user input choice
 #object called to play game
 game = BasketballGame(my_team, opponent_team, team_name)
@@ -72,10 +71,3 @@ def calculate_ppg(teamsWL):
         else:
             ppg = sum(teamsWL[team]['Scores']) / games_played
         print(f"{team}: {ppg}")
-	
-	
-	
-	
-
-
-
